1f2198bcd8f86835856fb43c506a6a01335ed36d
FabiannNi <fabian_ni@hotmail.com>
2016-05-14 20:43:35 +0800

    Update Util.java

--- Util.java
+++ Util.java
@@ -1,40 +1,49 @@
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Util {
 
-	public static String getIP() {
-		return null;
-	}
+    public static String getIP() {
+        return null;
+    }
 
-	public static String getCPUInfo() throws IOException {
-			Process process = Runtime.getRuntime().exec("wmic CPU get processorID");
-			process.getOutputStream();
-			Scanner sc = new Scanner(process.getInputStream());
-			String name = sc.next();
-			String serial = sc.next();
-			System.out.println(name+ "的cpu信息是： "+serial );
-			return serial;
-		
-	}
+    public static String getCPUInfo() throws IOException {
+        Process process = Runtime.getRuntime().exec("wmic CPU get processorID");
+        process.getOutputStream();
+        Scanner sc = new Scanner(process.getInputStream());
+        String name = sc.next();
+        String serial = sc.next();
+        System.out.println(name+ "的cpu信息是： "+serial );
+        return serial;
 
-	public static String getMac(String ip) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	public static String askCode(String macAdress, String cPUId) {
-		String askCode = "";
-		if(macAdress != null && !"".equals(macAdress)){
-		for(int i =0; i<macAdress.length();i++){
-			int number = macAdress.charAt(i);
-			if(number<=57&&number>=48)
-			 askCode += macAdress.charAt(i);
-		}
-		}
-		System.out.println(askCode);
+    public static String getMacAddress() throws IOException{
+        String command = "ifconfig -a";
+        String address = "";
+        Process p = Runtime.getRuntime().exec(command);
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = br.readLine();
+        if(line.indexOf("ether")>0){
+            int index = line.indexOf("r");
+            address = line.substring(index);
+        }
+        br.close();
+        return address.trim();
+    }
 
-		return askCode;
-	}
+    public static String askCode(String macAdress, String cPUId) {
+        String askCode = "";
+        if(macAdress != null && !"".equals(macAdress)){
+            for(int i =0; i<macAdress.length();i++){
+                int number = macAdress.charAt(i);
+                if(number<=57&&number>=48)
+                    askCode += macAdress.charAt(i);
+            }
+        }
+        System.out.println(askCode);
+
+        return askCode;
+    }
 
 }
